3b87014a8175f9af09216f38677c6dad5d74226a
Filip Nachov <fnachov@gmail.com>
2024-08-25 15:16:03 +0200

    removed the hello world program from the main file

--- main.asm
+++ main.asm
@@ -1,17 +1,5 @@
 section .data
-    hello db 'Hello World', 0x0A
-    helloLen equ $ - hello
 
 section .text
-    global _start
 
 start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, hello
-    mov rdx, helloLen
-    syscall
-
-    mov rax, 60
-    mov rdi, rdi
-    syscall
